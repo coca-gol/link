7a50f94cda009982feda547b8b5a783bd11c94d6
Xamer <xamerid@gmail.com>
2026-03-15 11:21:35 +0700

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@
     const convertFm = document.querySelector("#converter-button-format");
    //#const API = `${location.protocol}//${location.hostname}:3000`;
 
-    const API = "https://automation-dept-cash-invision.trycloudflare.com";
+    const API = "https://dan-locked-stat-group.trycloudflare.com";
     let converting = false;
     let current_Title = "media";
     
